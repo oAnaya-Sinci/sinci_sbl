c35ee51621edf0e417ebd50b14f69be6267f8968
Arturo Flores <aflores@sinci.com>
2020-03-31 16:46:30 -0600

    iconos

--- resources/views/plantilla/sidebaradministrador.blade.php
+++ resources/views/plantilla/sidebaradministrador.blade.php
@@ -8,13 +8,13 @@
                         Menu
                     </li>
                     <li class="nav-item nav-dropdown">
-                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-bag"></i> Configuracion</a>
+                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-pencil"></i> Configuracion</a>
                         <ul class="nav-dropdown-items">
                             <li @click="menu=1" class="nav-item">
-                                <a class="nav-link" href="#"><i class="icon-bag"></i> Maquinas</a>
+                                <a class="nav-link" href="#"><i class="icon-power"></i> Maquinas</a>
                             </li>
                             <li @click="menu=2" class="nav-item">
-                                <a class="nav-link" href="#"><i class="icon-bag"></i> Varibles</a>
+                                <a class="nav-link" href="#"><i class="icon-puzzle"></i> Varibles</a>
                             </li>
                         </ul>
                     </li>
