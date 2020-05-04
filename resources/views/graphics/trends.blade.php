b698beb6f9f961a68ac04e203dca18f70afa29e6
Jesus <jsandoval@sinci.com>
2020-05-04 15:59:37 -0500

    se agrega utils.js, trendsvars.js, y se edita la vista de trends para quemar ejemplo lineal chartjs

--- resources/views/graphics/trends.blade.php
+++ resources/views/graphics/trends.blade.php
@@ -8,7 +8,28 @@
               </h6>
             </div>
             <div class="card-body">
-              
+            
+    <div style="width:75%;">
+        <canvas id="canvas"></canvas>
+    </div>
+    <br>
+    <br>
+    <button id="randomizeData">Randomize Data</button>
+    <button id="addDataset">Add Dataset</button>
+    <button id="removeDataset">Remove Dataset</button>
+    <button id="addData">Add Data</button>
+    <button id="removeData">Remove Data</button>
+    
+
+
+
+
             </div>
           </div>
+@endsection
+@section('scripts')
+<script src="{{ asset('vendor/chart.js/chart.min.js')}}"></script>
+<script src="{{ asset('vendor/chart.js/chart.bundle.min.js')}}"></script>
+<script src="{{ asset('vendor/chart.js/utils.js')}}"></script>
+<script src="{{ asset('js/trendsvars.js')}}"></script>
 @endsection
