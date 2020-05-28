4e14ac8203eb830bce3c2e8877235d14773890be
Jesus <jsandoval@sinci.com>
2020-05-28 10:20:20 -0500

    SP consulta oee componentes

---
+++ database/migrations/2020_05_28_101416_sp_return_datos_oee_componentes.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SpReturnDatosOeeComponentes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared("DROP PROCEDURE IF EXISTS ConsultaOEEComponentes;");
+        DB::unprepared("CREATE  PROCEDURE `ConsultaOEEComponentes`(IN `_caso` VARCHAR(1), IN `_machId` INT, IN `_inpdate` varchar(10) )
+        NO SQL
+    IF (_caso = 'd') then    
+        select 
+            min(date_format(capturedTime, '%Y-%m-%d'))date,  
+            cast( sum(goodParts)as decimal(5,2)) GoodParts,
+            cast( sum(totalPieces)as decimal(5,2)) TotalParts,
+            cast( sum(totalPieces) - sum(GoodParts) as decimal(5,2)) BadParts,
+            cast( sum(runTime) as decimal(5,2)) RunningTime,
+            cast( sum(availableTime) as decimal(5,2)) AvailableTime,
+            cast( avg(ICT) as decimal(5,2)) ICT
+        from oee where date_format(capturedTime, '%Y-%m-%d') = _inpdate and idmachine = _machId ;
+                    
+    ELSEIF (_caso='m')then  /*Obtener los promedios de la medicion cada media hora*/
+        select 
+            min(date_format(capturedTime, '%Y-%m-%d'))date,  
+            cast( sum(goodParts)as decimal(5,2)) GoodParts,
+            cast( sum(totalPieces)as decimal(5,2)) TotalParts,
+            cast( sum(totalPieces) - sum(GoodParts) as decimal(5,2)) BadParts,
+            cast( sum(runTime) as decimal(5,2)) RunningTime,
+            cast( sum(availableTime) as decimal(5,2)) AvailableTime,
+            cast( avg(ICT) as decimal(5,2)) ICT
+        from oee where date_format(capturedTime, '%Y-%m') = date_format(concat( _inpdate ,'-01'),  '%Y-%m') and idmachine = _machId ;
+                    
+    ELSEIF (_caso='y')then
+        select 
+            min(date_format(capturedTime, '%Y-%m-%d'))date,  
+            cast( sum(goodParts)as decimal(5,2)) GoodParts,
+            cast( sum(totalPieces)as decimal(5,2)) TotalParts,
+            cast( sum(totalPieces) - sum(GoodParts) as decimal(5,2)) BadParts,
+            cast( sum(runTime) as decimal(5,2)) RunningTime,
+            cast( sum(availableTime) as decimal(5,2)) AvailableTime,
+            cast( avg(ICT) as decimal(5,2)) ICT
+        from oee  where date_format(capturedTime, '%Y' ) = date_format(concat( _inpdate ,'-01-01'),  '%Y') and idmachine = _machId;
+            
+                    
+    END IF");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
